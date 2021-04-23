390d616cdeebfd6a1caefa52e0ab2b9965f3528b
r-stemmle <ryan.stemmle@gmail.com>
2021-04-23 12:24:41 -0400

    Add distinct to flights passengers list to remover duplicates

--- spec/features/airlines/show_spec.rb
+++ spec/features/airlines/show_spec.rb
@@ -15,5 +15,21 @@ RSpec.describe "Airlines Show Page" do
       expect(page).to have_content(p2.name)
       expect(page).to have_content(p3.name)
     end
+
+    it "And I see that this list is unique" do
+      airline = Airline.create!(name: "TWA")
+      flight = airline.flights.create!(number: 123, date: '2021-05-12', departure_city: 'Denver', arrival_city: 'Reno')
+      p1 = Passenger.create!(name: "Joe", age: "23")
+      p2 = flight.passengers.create!(name: "Mary", age: "44")
+      p3 = flight.passengers.create!(name: "John", age: "55")
+      flight.passengers << p1
+      flight.passengers << p1
+
+      visit airline_path(airline)
+      expect(page).to have_content(p1.name, count: 1)
+      expect(page).to have_content(p2.name)
+      expect(page).to have_content(p3.name)
+      # save_and_open_page
+    end
   end
 end
