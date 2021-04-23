b4aa92b1dd18ba1b0d9f830e78f219e828511738
r-stemmle <ryan.stemmle@gmail.com>
2021-04-23 11:00:39 -0400

    Finish user story 1

--- spec/features/flights/index_spec.rb
+++ spec/features/flights/index_spec.rb
@@ -13,19 +13,24 @@ RSpec.describe "Flight Index Page" do
       f5 = united.flights.create!(number: 888, date: '2021-04-29', departure_city: 'Denver', arrival_city: 'Reno')
       f6 = twa.flights.create!(number: 999, date: '2021-05-11', departure_city: 'Denver', arrival_city: 'Reno')
       p1 = Passenger.create!(name: 'Ryan', age: 36)
-      
+      p2 = Passenger.create!(name: 'Joe', age: 16)
+      p3 = Passenger.create!(name: 'Kon', age: 26)
+      p4 = Passenger.create!(name: 'Kurt', age: 33)
+      p5 = Passenger.create!(name: 'Larry', age: 55)
+      p6 = Passenger.create!(name: 'Mary', age: 6)
+      p7 = Passenger.create!(name: 'Sue', age: 3)
+      f1.passengers << [p1, p2, p7]
+      f2.passengers << p5
+
       visit flights_path
 save_and_open_page
       expect(page).to have_content(f1.number)
       expect(page).to have_content(f1.airline.name)
+      expect(page).to have_content(p1.name)
       expect(page).to have_content(f2.number)
       expect(page).to have_content(f2.airline.name)
       expect(page).to have_content(f3.number)
     end
   end
-#   As a visitor
-# When I visit the flights index page
-# I see a list of all flight numbers
-# And next to each flight number I see the name of the Airline of that flight
-# And under each flight number I see the names of all that flight's passengers
+
 end
